85a7af6249fbc97875b27c4ecb59f4c1ba7c8f61
Daniel.Sendula <daniel.sendula@creative-sphere.com>
2016-01-16 18:42:14 +0000

    Updated adventure-start map, extracted tilea2.tsx and implemented collision

--- Projects/YumikosAdventure/YumikosAdventure.py
+++ Projects/YumikosAdventure/YumikosAdventure.py
@@ -15,9 +15,14 @@ global tilemap, playerPos, object, objects
 # it is called just *once*
 # at the beginning of our program
 def GameInit():
-    global screen, current_keys, screen_size, tilemap, player, playerPos, object, objects
+    global screen, current_keys, screen_size, tilemap, player
+    global playerPos, nextPlayerPos, playerCollideRect, nextPlayerCollideRect
+    global object, objects
+    global moved
 
-    screen_size = (640, 480)
+    moved = False
+
+    screen_size = (640, 640)
     pygame.init()
     screen = pygame.display.set_mode(screen_size)
 
@@ -29,8 +34,17 @@ def GameInit():
     object = objectsLayer.objects[0]
 
     player = pygame.image.load("images/player.png")
-    playerPos = pygame.Rect(screen_size[0] / 2 - player.get_width() / 2, screen_size[1] / 2 - player.get_height() / 2,
-                            player.get_width(), player.get_height())
+    # playerPos = pygame.Rect(screen_size[0] / 2 - player.get_width() / 2,
+    #                         screen_size[1] / 2 - player.get_height() / 2,
+    #                         player.get_width(), player.get_height())
+
+    playerPos = pygame.Rect(200, 240, player.get_width(), player.get_height())
+    playerCollideRect = playerPos.move(8, 8)
+    playerCollideRect.width = playerCollideRect.width - 16
+    playerCollideRect.height = playerCollideRect.height - 16
+
+    nextPlayerPos = playerPos.copy()
+    nextPlayerCollideRect = playerCollideRect.copy()
 
 # == GameReset ==
 # Put 'new game' starting values here.
@@ -47,32 +61,122 @@ def GameReset():
 # Here is where we will put the keyboard responses, 
 # collision detection and other game logic.
 def UpdateGameScreen(elapsed_ms):
-    global map_offset, tilemap, playerPos
+    global map_offset, tilemap
+    global playerPos, nextPlayerPos, playerCollideRect, nextPlayerCollideRect
+    global moved
 
-    # Make a copy of the current position
-    # Move it according to the pressed keys
-    # Check the new centre location against the collision
-    # map
-    # If no collision, then update map position
-    if current_keys[pygame.K_UP] and map_offset[1] > 0:
-        #map_offset[1] -= elapsed_ms
-        playerPos[1] -= elapsed_ms
+    objectsLayer = tilemap.layers['objects']
+    groundLayer = tilemap.layers['ground']
 
-    if current_keys[pygame.K_DOWN]:
-        # map_offset[1] += elapsed_ms
-        playerPos[1] += elapsed_ms
+    speed = elapsed_ms / 2
 
+    nextPlayerPos[0] = playerPos[0]
+    nextPlayerPos[1] = playerPos[1]
+    nextPlayerCollideRect[0] = playerCollideRect[0]
+    nextPlayerCollideRect[1] = playerCollideRect[1]
+
+    moved = False;
     if current_keys[pygame.K_LEFT] and map_offset[0] > 0:
-        # map_offset[0] -= elapsed_ms
-        playerPos[0] -= elapsed_ms
+        nextPlayerPos[0] -= speed
+        nextPlayerCollideRect[0] -= speed
+        moved = True
+    elif current_keys[pygame.K_RIGHT]:
+        nextPlayerPos[0] += speed
+        nextPlayerCollideRect[0] += speed
+        moved = True
 
-    if current_keys[pygame.K_RIGHT]:
-        # map_offset[0] += elapsed_ms
-        playerPos[0] += elapsed_ms
+    if moved:
+        collisionCells = groundLayer.get_in_region(nextPlayerPos.left, nextPlayerPos.top, nextPlayerPos.right, nextPlayerPos.bottom)
 
-    # tilemap.set_focus(map_offset[0], map_offset[1])
+        moved = processTilesCollision(collisionCells, nextPlayerCollideRect)
+
+        if moved:
+            moved = processObjectCollision(objects, objectsLayer)
+
+        if moved:
+            playerPos[0] = nextPlayerPos[0]
+            playerCollideRect[0] = nextPlayerCollideRect[0]
+        else:
+            nextPlayerPos[0] = playerPos[0]
+            nextPlayerCollideRect[0] = playerCollideRect[0]
+
+    moved = False
+    if current_keys[pygame.K_UP] and map_offset[1] > 0:
+        nextPlayerPos[1] -= speed
+        nextPlayerCollideRect[1] -= speed
+        moved = True
+    elif current_keys[pygame.K_DOWN]:
+        nextPlayerPos[1] += speed
+        nextPlayerCollideRect[1] += speed
+        moved = True
+
+    if moved:
+        collisionCells = groundLayer.get_in_region(nextPlayerPos.left, nextPlayerPos.top, nextPlayerPos.right, nextPlayerPos.bottom)
+
+        moved = processTilesCollision(collisionCells, nextPlayerCollideRect)
+
+        if moved:
+            moved = processObjectCollision(objects, objectsLayer)
+
+        if moved:
+            playerPos[1] = nextPlayerPos[1]
+            playerCollideRect[1] = nextPlayerCollideRect[1]
+        else:
+            nextPlayerPos[1] = playerPos[1]
+            nextPlayerCollideRect[1] = playerCollideRect[1]
+
     tilemap.set_focus(playerPos[0], playerPos[1])
     tilemap.update(elapsed_ms)
+
+
+def processObjectCollision(objects, objectsLayer):
+    objects = objectsLayer.collide(nextPlayerCollideRect, "OnCollision")
+    oi = 0
+    oiLen = len(objects)
+    while moved and oi < oiLen:
+        object = objects[oi]
+        if "OnCollision" in object.properties:
+            exec(object.properties["OnCollision"])
+        oi = oi + 1
+
+    return moved
+
+
+def processTilesCollision(collisionCells, nextPlayerCollideRect):
+    moved = True
+
+    if len(collisionCells) > 0:
+        cc = 0
+        ccLen = len(collisionCells)
+        while moved and cc < ccLen:
+            cell = collisionCells[cc]
+            tile = cell.tile
+            collisionRects = tile.collisionRects;
+            cr = 0
+            crLen = len(collisionRects)
+            while moved and cr < crLen:
+                if collideWithOffset(nextPlayerCollideRect, cell.px, cell.py, collisionRects[cr]):
+                    if "OnCollision" in tile.properties:
+                        exec(tile.properties["OnCollision"])
+                    else:
+                        moved = False
+                cr = cr + 1
+
+            cc = cc + 1
+
+    return moved
+
+def collideWithOffset(r1, x, y, r2):
+    # return (   A->x < B->x + B->w
+    #         && A->y < B->y + B->h
+    #         && A->x + A->w > B->x
+    #         && A->y + A->h > B->y)
+
+
+    return r1.x < r2.x + x + r2.w and r1.y < r2.y + y + r2.h and r1.x + r1.w > r2.x + x and r1.y + r1.h > r2.y + y
+
+def preventMovement():
+    moved = False
 
 # == DrawGameScreen ==
 # This function is called once every game loop
@@ -82,8 +186,10 @@ def UpdateGameScreen(elapsed_ms):
 def DrawGameScreen():
     global screen, tilemap, player, playerPos
     tilemap.draw(screen)
-    pos = playerPos.move(-tilemap.viewport.x, -tilemap.viewport.y)
-    screen.blit(player, pos)
+
+    nextPlayerPos.move_ip(-tilemap.viewport.x, -tilemap.viewport.y)
+
+    screen.blit(player, nextPlayerPos)
 
 # == GameLoop ==
 # Put things that have to occur repeatedly
@@ -107,42 +213,42 @@ def GameLoop():
     # flip the screen to show our drawing
     pygame.display.flip()
 
-    pos = playerPos.move(tilemap.viewport.x, tilemap.viewport.y)
-
-    groundLayer = tilemap.layers['ground']
-
-    collisionCells = groundLayer.get_in_region(pos.left, pos.top, pos.right, pos.bottom)
-
-    if len(collisionCells) > 0:
-        for cell in collisionCells:
-            tile = cell.tile
-            print(", " + str(tile.gid), end="")
-        print("")
-
-        colRects = collisionCells[0].tile.collisionRects
-        if len(colRects) > 0:
-            rect = colRects[0]
-            print("Got rect[" + str(rect.left) + ", " + str(rect.top) + "," + str(rect.width) + "," + str(rect.height) + "]")
-
-    for cell in collisionCells:
-        tile = cell.tile
-        i = pos.collidelist(tile.collisionRects)
-        if i >= 0:
-            print("found collision " + str(i))
-
-    # for tile in collisionTiles:
-    #     if tile
-
-    objectsLayer = tilemap.layers['objects']
-
-
-    objects = objectsLayer.collide(pos, "OnCollision")
-
-    # print("x=" + str(pos[0]) + ", y=" + str(pos[1]) + " o# " + str(len(objects)))
-
-    for object in objects:
-        onCollision = object.properties["OnCollision"]
-        exec(onCollision)
+    # pos = playerPos.move(tilemap.viewport.x, tilemap.viewport.y)
+    #
+    # groundLayer = tilemap.layers['ground']
+    #
+    # collisionCells = groundLayer.get_in_region(pos.left, pos.top, pos.right, pos.bottom)
+    #
+    # if len(collisionCells) > 0:
+    #     for cell in collisionCells:
+    #         tile = cell.tile
+    #         print(", " + str(tile.gid), end="")
+    #     print("")
+    #
+    #     colRects = collisionCells[0].tile.collisionRects
+    #     if len(colRects) > 0:
+    #         rect = colRects[0]
+    #         print("Got rect[" + str(rect.left) + ", " + str(rect.top) + "," + str(rect.width) + "," + str(rect.height) + "]")
+    #
+    # for cell in collisionCells:
+    #     tile = cell.tile
+    #     i = pos.collidelist(tile.collisionRects)
+    #     if i >= 0:
+    #         print("found collision " + str(i))
+    #
+    # # for tile in collisionTiles:
+    # #     if tile
+    #
+    # objectsLayer = tilemap.layers['objects']
+    #
+    #
+    # objects = objectsLayer.collide(pos, "OnCollision")
+    #
+    # # print("x=" + str(pos[0]) + ", y=" + str(pos[1]) + " o# " + str(len(objects)))
+    #
+    # for object in objects:
+    #     onCollision = object.properties["OnCollision"]
+    #     exec(onCollision)
 
 # == (3) Call the functions to run the game == 
 # We have only *defined* our functions above.
---
+++ Projects/YumikosAdventure/tilea2.tsx
@@ -0,0 +1,157 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="tilea2" tilewidth="16" tileheight="16">
+ <image source="maps/tilea2.png" width="512" height="384"/>
+ <terraintypes>
+  <terrain name="New Terrain" tile="-1"/>
+ </terraintypes>
+ <tile id="4">
+  <objectgroup draworder="index">
+   <object id="0" x="12.2825" y="0.307062" width="3.27533" height="15.4214"/>
+  </objectgroup>
+ </tile>
+ <tile id="5">
+  <objectgroup draworder="index">
+   <object id="0" x="0.204708" y="0.17059" width="3.54828" height="15.6602"/>
+  </objectgroup>
+ </tile>
+ <tile id="6">
+  <objectgroup draworder="index">
+   <object id="0" x="0.17059" y="0.102354" width="3.54828" height="15.5919"/>
+   <object id="0" x="4.1624" y="9.10952" width="7.7448" height="4.67417"/>
+   <object id="0" x="12.3166" y="0.238826" width="3.30945" height="15.4555"/>
+  </objectgroup>
+ </tile>
+ <tile id="7">
+  <objectgroup draworder="index">
+   <object id="0" x="0.238826" y="0.272944" width="3.54828" height="15.5237"/>
+   <object id="0" x="4.29887" y="9.31423" width="7.47185" height="4.23064"/>
+   <object id="0" x="12.2825" y="0.17059" width="3.51416" height="15.5237"/>
+  </objectgroup>
+ </tile>
+ <tile id="36">
+  <objectgroup draworder="index">
+   <object id="0" x="12.3849" y="0.238826" width="3.37769" height="15.3531"/>
+  </objectgroup>
+ </tile>
+ <tile id="37">
+  <objectgroup draworder="index">
+   <object id="0" x="0.204708" y="0.238826" width="3.48004" height="15.6602"/>
+  </objectgroup>
+ </tile>
+ <tile id="38">
+  <objectgroup draworder="index">
+   <object id="0" x="0.17059" y="0.17059" width="3.51416" height="15.5919"/>
+   <object id="0" x="4.29887" y="3.13886" width="7.50597" height="4.50358"/>
+   <object id="0" x="12.2484" y="0.17059" width="3.51416" height="15.5578"/>
+  </objectgroup>
+ </tile>
+ <tile id="39">
+  <objectgroup draworder="index">
+   <object id="0" x="0.238826" y="0.17059" width="3.51416" height="15.6261"/>
+   <object id="0" x="4.26476" y="3.27533" width="7.64244" height="4.46946"/>
+   <object id="0" x="12.2143" y="0.102354" width="3.61651" height="15.7284"/>
+  </objectgroup>
+ </tile>
+ <tile id="68">
+  <objectgroup draworder="index">
+   <object id="0" x="12.5896" y="0.375299" width="3.17298" height="15.5578"/>
+  </objectgroup>
+ </tile>
+ <tile id="69">
+  <objectgroup draworder="index">
+   <object id="0" x="0.272944" y="0.102354" width="3.54828" height="15.5919"/>
+   <object id="0" x="4.23064" y="9.17776" width="7.57421" height="4.5377"/>
+   <object id="0" x="12.3166" y="0.409417" width="3.48004" height="15.4896"/>
+  </objectgroup>
+ </tile>
+ <tile id="70">
+  <objectgroup draworder="index">
+   <object id="0" x="0.102354" y="0.204708" width="3.61651" height="15.4555"/>
+   <object id="0" x="4.23064" y="9.17776" width="7.64244" height="4.50358"/>
+   <object id="0" x="12.419" y="0.307062" width="3.34357" height="15.4555"/>
+  </objectgroup>
+ </tile>
+ <tile id="71">
+  <objectgroup draworder="index">
+   <object id="0" x="0.17059" y="0.17059" width="3.61651" height="15.7284"/>
+  </objectgroup>
+ </tile>
+ <tile id="100">
+  <objectgroup draworder="index">
+   <object id="0" x="12.3507" y="0.34118" width="3.10474" height="15.319"/>
+  </objectgroup>
+ </tile>
+ <tile id="101">
+  <objectgroup draworder="index">
+   <object id="0" x="0.102354" y="0.272944" width="3.61651" height="15.7284"/>
+   <object id="0" x="4.29887" y="3.13886" width="7.43773" height="4.67417"/>
+   <object id="0" x="12.2143" y="0.17059" width="3.51416" height="15.6943"/>
+  </objectgroup>
+ </tile>
+ <tile id="102">
+  <objectgroup draworder="index">
+   <object id="0" x="0.272944" y="0.17059" width="3.48004" height="15.4896"/>
+   <object id="0" x="4.36711" y="3.13886" width="7.47185" height="4.43535"/>
+   <object id="0" x="12.3166" y="0.238826" width="3.37769" height="15.4214"/>
+  </objectgroup>
+ </tile>
+ <tile id="103">
+  <objectgroup draworder="index">
+   <object id="0" x="0.136472" y="0.17059" width="3.65063" height="15.6602"/>
+  </objectgroup>
+ </tile>
+ <tile id="132">
+  <objectgroup draworder="index">
+   <object id="0" x="12.3507" y="0.238826" width="3.00239" height="15.4214"/>
+  </objectgroup>
+ </tile>
+ <tile id="133">
+  <objectgroup draworder="index">
+   <object id="0" x="0.17059" y="0.17059" width="3.68475" height="15.6261"/>
+   <object id="0" x="4.23064" y="9.21187" width="7.60832" height="4.67417"/>
+   <object id="0" x="12.2825" y="0.136472" width="3.51416" height="15.4896"/>
+  </objectgroup>
+ </tile>
+ <tile id="134">
+  <objectgroup draworder="index">
+   <object id="0" x="0.136472" y="0.17059" width="3.71887" height="15.6943"/>
+   <object id="0" x="4.12828" y="9.14364" width="7.81303" height="4.70829"/>
+   <object id="0" x="12.2484" y="0.17059" width="3.5824" height="15.5237"/>
+  </objectgroup>
+ </tile>
+ <tile id="135">
+  <objectgroup draworder="index">
+   <object id="0" x="0.136472" y="0.136472" width="3.71887" height="15.7284"/>
+  </objectgroup>
+ </tile>
+ <tile id="164">
+  <objectgroup draworder="index">
+   <object id="0" x="12.2143" y="0.204708" width="3.44592" height="15.4555"/>
+  </objectgroup>
+ </tile>
+ <tile id="165">
+  <objectgroup draworder="index">
+   <object id="0" x="0.204708" y="0.136472" width="3.48004" height="15.4555"/>
+   <object id="0" x="4.29887" y="3.44592" width="7.47185" height="4.1624"/>
+   <object id="0" x="12.4872" y="0.238826" width="3.24121" height="15.319"/>
+  </objectgroup>
+ </tile>
+ <tile id="166">
+  <objectgroup draworder="index">
+   <object id="0" x="0.375299" y="0.238826" width="3.51416" height="15.4896"/>
+   <object id="0" x="4.29887" y="3.17298" width="7.60832" height="4.50358"/>
+   <object id="0" x="12.3507" y="0.204708" width="3.51416" height="15.5578"/>
+  </objectgroup>
+ </tile>
+ <tile id="167">
+  <objectgroup draworder="index">
+   <object id="0" x="0.204708" y="0.102354" width="3.51416" height="15.9672"/>
+  </objectgroup>
+ </tile>
+ <tile id="204">
+  <objectgroup draworder="index">
+   <object id="0" x="0.625" y="0.375" width="15.375" height="5.25"/>
+   <object id="0" x="0" y="0" width="5.25" height="15.5"/>
+  </objectgroup>
+ </tile>
+</tileset>
