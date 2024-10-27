de7adf1d3574ad6418c30d1f797570d077797070
Daniel Sendula <natdan@users.noreply.github.com>
2024-10-27 19:03:10 +0000

    Fixed a few issues and added more stuff

--- Projects/YumikosAdventure/assets/objects.tsx
+++ Projects/YumikosAdventure/assets/objects.tsx
@@ -1,11 +1,12 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="objects" tilewidth="32" tileheight="32">
+<tileset version="1.11" tiledversion="1.11.0" name="objects" tilewidth="32" tileheight="32" tilecount="128" columns="8">
  <image source="images/objects.png" width="256" height="512"/>
  <tile id="0">
   <properties>
-   <property name="OnCollision">AddCoins(1)
-RemoveCollidedObject(); Say(&quot;Got one coin&quot;)</property>
    <property name="name" value="coin"/>
+   <property name="on_collision">add_object_to_inventory(obj)
+remove_collided_object()
+say(&quot;Got one coin&quot;)</property>
   </properties>
  </tile>
  <tile id="2">
@@ -25,22 +26,27 @@ RemoveCollidedObject(); Say(&quot;Got one coin&quot;)</property>
  </tile>
  <tile id="8">
   <properties>
-   <property name="Name" value="door-up"/>
+   <property name="name" value="door-up"/>
   </properties>
  </tile>
  <tile id="9">
   <properties>
-   <property name="Name" value="door-down"/>
+   <property name="name" value="door-down"/>
   </properties>
  </tile>
  <tile id="10">
   <properties>
-   <property name="Name" value="door-left"/>
+   <property name="name" value="door-left"/>
   </properties>
  </tile>
  <tile id="11">
   <properties>
-   <property name="Name" value="door-right"/>
+   <property name="name" value="door-right"/>
+  </properties>
+ </tile>
+ <tile id="16">
+  <properties>
+   <property name="name" value="key"/>
   </properties>
  </tile>
 </tileset>
--- Projects/YumikosAdventure/yumikos_adventue_main.py
+++ Projects/YumikosAdventure/yumikos_adventue_main.py
@@ -6,21 +6,35 @@ from engine.level import Level
 
 from engine.debug import (Debug)
 from game.top_down_game_context import TopDownGameContext
+from yumiko_game import YumikoGame
 
 screen_size = (1024, 640)
 
 pygame.init()
 pygame.font.init()
 
+small_font = pygame.font.SysFont("apple casual", 16)
+font = pygame.font.SysFont("apple casual", 24)
 
 screen = pygame.display.set_mode(screen_size)
 screen_rect = screen.get_rect()
 
-levels = Level.load_levels(screen_rect, "assets/start.tmx")
-game_context = TopDownGameContext(levels)
+levels = Level.load_levels(
+    screen_rect,
+    "assets/start.tmx",
+    "assets/example-map.tmx"
+)
+
+game_context = YumikoGame(levels, font, small_font)
 game_context.set_level(levels["start"])
 
-game = Game(screen, game_context, 60, True)
+game = Game(
+    screen,
+    game_context,
+    60, True)
+
+game.before_map = None
+game.after_map = game_context.after_map
 
 game.main_loop()
 
