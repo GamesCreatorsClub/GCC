b10fa7d6f66eb8860a9ae1a2856fb2ecb2bbd3f2
Daniel Sendula <natdan@users.noreply.github.com>
2024-10-29 17:18:01 +0000

    Updated maps

--- Projects/YumikosAdventure/assets/tileb.tsx
+++ Projects/YumikosAdventure/assets/tileb.tsx
@@ -1,9 +1,46 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="tileb" tilewidth="32" tileheight="32" tilecount="256" columns="16">
+<tileset version="1.11" tiledversion="1.11.0" name="tileb" tilewidth="32" tileheight="32" tilecount="256" columns="16">
  <image source="maps/tileb.png" width="512" height="512"/>
- <terraintypes>
-  <terrain name="mound" tile="-1"/>
- </terraintypes>
+ <tile id="0"/>
+ <tile id="1"/>
+ <tile id="2"/>
+ <tile id="3"/>
+ <tile id="4"/>
+ <tile id="5"/>
+ <tile id="6"/>
+ <tile id="7"/>
+ <tile id="8"/>
+ <tile id="9"/>
+ <tile id="10"/>
+ <tile id="11"/>
+ <tile id="12"/>
+ <tile id="13"/>
+ <tile id="14"/>
+ <tile id="15"/>
+ <tile id="16"/>
+ <tile id="17"/>
+ <tile id="18"/>
+ <tile id="19"/>
+ <tile id="20"/>
+ <tile id="21"/>
+ <tile id="22"/>
+ <tile id="23"/>
+ <tile id="24"/>
+ <tile id="25"/>
+ <tile id="26"/>
+ <tile id="27"/>
+ <tile id="28"/>
+ <tile id="29"/>
+ <tile id="30"/>
+ <tile id="31"/>
+ <tile id="32"/>
+ <tile id="33"/>
+ <tile id="34"/>
+ <tile id="35"/>
+ <tile id="36"/>
+ <tile id="37"/>
+ <tile id="38"/>
+ <tile id="39"/>
  <tile id="40">
   <properties>
    <property name="name" value="chest-closed"/>
@@ -14,6 +51,8 @@
    <property name="name" value="chest-open"/>
   </properties>
  </tile>
+ <tile id="42"/>
+ <tile id="43"/>
  <tile id="44">
   <objectgroup draworder="index">
    <object id="0" x="2.96077" y="1.94301" width="26.832" height="26.3694">
@@ -21,6 +60,42 @@
    </object>
   </objectgroup>
  </tile>
+ <tile id="45"/>
+ <tile id="46"/>
+ <tile id="47"/>
+ <tile id="48"/>
+ <tile id="49"/>
+ <tile id="50"/>
+ <tile id="51"/>
+ <tile id="52"/>
+ <tile id="53"/>
+ <tile id="54"/>
+ <tile id="55"/>
+ <tile id="56"/>
+ <tile id="57"/>
+ <tile id="58"/>
+ <tile id="59"/>
+ <tile id="60"/>
+ <tile id="61"/>
+ <tile id="62"/>
+ <tile id="63"/>
+ <tile id="64"/>
+ <tile id="65"/>
+ <tile id="66"/>
+ <tile id="67"/>
+ <tile id="68"/>
+ <tile id="69"/>
+ <tile id="70"/>
+ <tile id="71"/>
+ <tile id="72"/>
+ <tile id="73"/>
+ <tile id="74"/>
+ <tile id="75"/>
+ <tile id="76"/>
+ <tile id="77"/>
+ <tile id="78"/>
+ <tile id="79"/>
+ <tile id="80"/>
  <tile id="81">
   <properties>
    <property name="name" value="sign"/>
@@ -30,30 +105,209 @@
    <object id="0" x="13.0035" y="24.2538" width="5.84428" height="7.52451"/>
   </objectgroup>
  </tile>
+ <tile id="82"/>
+ <tile id="83"/>
+ <tile id="84"/>
+ <tile id="85"/>
+ <tile id="86"/>
+ <tile id="87"/>
+ <tile id="88"/>
+ <tile id="89"/>
+ <tile id="90"/>
+ <tile id="91"/>
+ <tile id="92"/>
+ <tile id="93"/>
+ <tile id="94"/>
+ <tile id="95"/>
+ <tile id="96"/>
+ <tile id="97"/>
+ <tile id="98"/>
+ <tile id="99"/>
+ <tile id="100"/>
+ <tile id="101"/>
+ <tile id="102"/>
+ <tile id="103"/>
+ <tile id="104"/>
+ <tile id="105"/>
+ <tile id="106"/>
+ <tile id="107"/>
+ <tile id="108"/>
+ <tile id="109"/>
+ <tile id="110"/>
+ <tile id="111"/>
+ <tile id="112"/>
+ <tile id="113"/>
+ <tile id="114"/>
+ <tile id="115"/>
+ <tile id="116"/>
+ <tile id="117"/>
+ <tile id="118"/>
+ <tile id="119"/>
+ <tile id="120"/>
+ <tile id="121"/>
+ <tile id="122"/>
+ <tile id="123"/>
+ <tile id="124"/>
+ <tile id="125"/>
+ <tile id="126"/>
+ <tile id="127"/>
+ <tile id="128"/>
+ <tile id="129"/>
+ <tile id="130"/>
+ <tile id="131"/>
+ <tile id="132"/>
+ <tile id="133"/>
+ <tile id="134"/>
+ <tile id="135"/>
+ <tile id="136"/>
+ <tile id="137"/>
+ <tile id="138"/>
+ <tile id="139"/>
+ <tile id="140"/>
+ <tile id="141"/>
+ <tile id="142"/>
+ <tile id="143"/>
+ <tile id="144"/>
+ <tile id="145"/>
+ <tile id="146"/>
+ <tile id="147"/>
+ <tile id="148"/>
+ <tile id="149"/>
+ <tile id="150"/>
+ <tile id="151"/>
+ <tile id="152"/>
+ <tile id="153"/>
+ <tile id="154"/>
+ <tile id="155"/>
+ <tile id="156"/>
+ <tile id="157"/>
+ <tile id="158"/>
+ <tile id="159"/>
+ <tile id="160"/>
+ <tile id="161"/>
+ <tile id="162"/>
+ <tile id="163"/>
+ <tile id="164"/>
+ <tile id="165"/>
+ <tile id="166"/>
+ <tile id="167"/>
+ <tile id="168"/>
+ <tile id="169"/>
+ <tile id="170"/>
+ <tile id="171"/>
+ <tile id="172"/>
+ <tile id="173"/>
+ <tile id="174"/>
+ <tile id="175"/>
+ <tile id="176"/>
+ <tile id="177"/>
+ <tile id="178"/>
+ <tile id="179"/>
+ <tile id="180"/>
+ <tile id="181"/>
+ <tile id="182"/>
+ <tile id="183"/>
+ <tile id="184"/>
+ <tile id="185"/>
+ <tile id="186"/>
+ <tile id="187"/>
+ <tile id="188"/>
+ <tile id="189"/>
+ <tile id="190"/>
+ <tile id="191"/>
+ <tile id="192"/>
+ <tile id="193"/>
+ <tile id="194"/>
+ <tile id="195"/>
+ <tile id="196"/>
+ <tile id="197"/>
+ <tile id="198"/>
+ <tile id="199"/>
+ <tile id="200"/>
+ <tile id="201"/>
+ <tile id="202"/>
+ <tile id="203"/>
+ <tile id="204"/>
+ <tile id="205"/>
+ <tile id="206"/>
+ <tile id="207"/>
+ <tile id="208"/>
+ <tile id="209"/>
+ <tile id="210"/>
+ <tile id="211"/>
+ <tile id="212"/>
+ <tile id="213"/>
+ <tile id="214"/>
+ <tile id="215"/>
+ <tile id="216"/>
+ <tile id="217"/>
+ <tile id="218"/>
  <tile id="230">
   <properties>
    <property name="name" value="plants-killer-1"/>
   </properties>
+  <animation>
+   <frame tileid="230" duration="300"/>
+   <frame tileid="246" duration="300"/>
+  </animation>
  </tile>
  <tile id="246">
   <properties>
    <property name="name" value="plants-killer-2"/>
   </properties>
  </tile>
- <tile id="219" terrain=",,,0"/>
- <tile id="220" terrain=",,0,0"/>
- <tile id="221" terrain=",,0,"/>
- <tile id="235" terrain=",0,,0"/>
- <tile id="236" terrain="0,0,0,0"/>
- <tile id="237" terrain="0,,0,"/>
- <tile id="238" terrain=",0,0,0"/>
- <tile id="239" terrain="0,,0,0"/>
- <tile id="251" terrain=",0,,"/>
- <tile id="252" terrain="0,0,,"/>
- <tile id="253" terrain="0,,,"/>
- <tile id="254" terrain="0,0,,0"/>
- <tile id="255" terrain="0,0,0,"/>
-<wangsets>
+ <tile id="219"/>
+ <tile id="222"/>
+ <tile id="223"/>
+ <tile id="224"/>
+ <tile id="225"/>
+ <tile id="226"/>
+ <tile id="227"/>
+ <tile id="228"/>
+ <tile id="229"/>
+ <tile id="220"/>
+ <tile id="231"/>
+ <tile id="232"/>
+ <tile id="233"/>
+ <tile id="234"/>
+ <tile id="221"/>
+ <tile id="235"/>
+ <tile id="236"/>
+ <tile id="237"/>
+ <tile id="238"/>
+ <tile id="240"/>
+ <tile id="241"/>
+ <tile id="242"/>
+ <tile id="243"/>
+ <tile id="244"/>
+ <tile id="245"/>
+ <tile id="239"/>
+ <tile id="247"/>
+ <tile id="248"/>
+ <tile id="249"/>
+ <tile id="250"/>
+ <tile id="251"/>
+ <tile id="252"/>
+ <tile id="253"/>
+ <tile id="254"/>
+ <tile id="255"/>
+ <wangsets>
+  <wangset name="Terrains" type="corner" tile="-1">
+   <wangcolor name="mound" color="#ff0000" tile="-1" probability="1"/>
+   <wangtile tileid="219" wangid="0,0,0,1,0,0,0,0"/>
+   <wangtile tileid="220" wangid="0,0,0,1,0,1,0,0"/>
+   <wangtile tileid="221" wangid="0,0,0,0,0,1,0,0"/>
+   <wangtile tileid="235" wangid="0,1,0,1,0,0,0,0"/>
+   <wangtile tileid="236" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="237" wangid="0,0,0,0,0,1,0,1"/>
+   <wangtile tileid="238" wangid="0,1,0,1,0,1,0,0"/>
+   <wangtile tileid="239" wangid="0,0,0,1,0,1,0,1"/>
+   <wangtile tileid="251" wangid="0,1,0,0,0,0,0,0"/>
+   <wangtile tileid="252" wangid="0,1,0,0,0,0,0,1"/>
+   <wangtile tileid="253" wangid="0,0,0,0,0,0,0,1"/>
+   <wangtile tileid="254" wangid="0,1,0,1,0,0,0,1"/>
+   <wangtile tileid="255" wangid="0,1,0,0,0,1,0,1"/>
+  </wangset>
   <wangset name="Terrains" type="corner" tile="-1">
    <wangcolor name="mound" color="#ff0000" tile="-1" probability="1"/>
    <wangtile tileid="219" wangid="0,0,0,1,0,0,0,0"/>
